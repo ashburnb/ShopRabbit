6209f0e5bd8f65246da412186f4ddb87166ada1d
Brad <bradleyashburn@icloud.com>
2023-02-16 21:42:19 -1000

    Initial Commit

---
+++ Brad_Ashburn_Shopping_App/Brad_Ashburn_Shopping_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Brad_Ashburn_Shopping_AppApp.swift
+//  Brad_Ashburn_Shopping_App
+//
+//  Created by Brad Ashburn on 2/16/23.
+//
+
+import SwiftUI
+
+@main
+struct Brad_Ashburn_Shopping_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
